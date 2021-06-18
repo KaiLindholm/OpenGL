f9e7e0d9df123b5873fd5305b5d699c4f9d3da05
Kai Lindholm <kailindolm20022@gmail.com>
2021-06-17 23:43:55 -0500

    Modern openGL Init check Open GL Version: 4.6.0 NVIDIA 466.47

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,4 +1,8 @@
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
 
 int main(void)
 {
@@ -8,6 +12,7 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -19,13 +24,17 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f); 
+        glVertex2f(-0.5f, -0.5f); /* creates a triangle with three vertices */
         glVertex2f(0.0f, 0.5f);
         glVertex2f(0.5f, -0.5f);
         glEnd();
